b5524fd32c3cdc7edb5a9c7835d8220e82b52399
Murat <murattufekcioglu@hotmail.com>
2021-01-27 18:00:40 +0300

    Textarea'yı kaldırdım, birisi contenteditable, iki div ekledim. Ekran döndürme ile etkin div değişiyor.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 
 window.onorientationchange = function (event) {
     console.log("the orientation of the device is now " + event.target.screen.orientation.angle);
-
+    var textinput = this.document.getElementById("textinput");
+    var textoutput = this.document.getElementById("textoutput")
     if (event.target.screen.orientation.angle == 0) {
-
+        textoutput.style.display = "none"
+        textinput.style.display = "inline"
     } else if (event.target.screen.orientation.angle == 90) {
-
+        textoutput.textContent = textinput.textContent
+        textoutput.style.display = "inline"
+        textinput.style.display = "none"
     }
 };
