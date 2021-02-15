aed24a686b003a48457740c264adeb259eb05484
Murat Tüfekçioğlu <murattufekcioglu@hotmail.com>
2021-02-15 19:24:54 +0300

    offline denemeleri yapıyorum .1

--- typetilt/service-worker.js
+++ typetilt/service-worker.js
@@ -1,7 +1,37 @@
 // Listen for install event, set callback
-self.addEventListener('install', function(event) {
-    // Perform some task
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("[Service Worker] Caching all: app shell and content");
+      return cache.addAll(contentToCache);
+    })
+  );
 });
-self.addEventListener('activate', function(event) {
-    // Perform some task
+
+var cacheName = "appCache-v1";
+var appShellFiles = ["index.html", "app.js", "style.css", "icons/phone.svg"];
+
+self.addEventListener("activate", function (event) {
+  console.log("[Service Worker] Activate");
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      console.log("[Service Worker] Fetching resource: " + e.request.url);
+      return (
+        r ||
+        fetch(e.request).then((response) => {
+          return caches.open(cacheName).then((cache) => {
+            console.log(
+              "[Service Worker] Caching new resource: " + e.request.url
+            );
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        })
+      );
+    })
+  );
 });
